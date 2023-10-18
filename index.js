a2b660362420e661f968b9fc1585333f302641cf
Mona <mona-rea.nerva@voco.ee>
2023-10-18 13:58:00 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const fs = require('fs');
 
 // use ejs files to prepare templates for views
 const path = require('path')
@@ -7,9 +8,18 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-	res.render('index')
+	// get data from file
+	fs.readFile('./tasks', 'utf8', (err, data) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		// task list data from file
+		const tasks = data.split("\n")
+		res.render('index',  {tasks: tasks})
+	});
 })
 
-app.listen(3003, () => {
-	console.log('Example app is started at http://localhost:3003')
+app.listen(3005, () => {
+	console.log('Example app is started at http://localhost:3005')
 })
